88c026bde6c204772149ae9bec10cbee0536dbe9
Anshad UK <anshadu@gmail.com>
2024-02-21 17:25:18 +0530

    updated

--- Todo-App/src/components/Card.jsx
+++ Todo-App/src/components/Card.jsx
@@ -12,7 +12,7 @@ const Card = () => {
   
   return (
     <div className="w-1/2 h-3/4 min-h-max bg-amber-100 shadow-2xl rounded-lg p-2 items-center flex flex-col space-y-10 justify-between">
-      <div className="flex flex-col space-y-10 w-full h-3/4 min-h-max items-center">
+      <div className="flex flex-col space-y-4 w-full h-3/4 min-h-max items-center">
         <h1 className="text-3xl font-semibold underline">My Todo List</h1>
         <div className="w-3/4">
           {toggleForm? <AddTodoForm />
@@ -33,8 +33,8 @@ const Card = () => {
             ))}
           </ul>
         </div>
+        <button onClick={()=>dispatch(todosCleared())} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-10 rounded focus:outline-none focus:shadow-outline">Clear</button>
       </div>
-      <button onClick={()=>dispatch(todosCleared())} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-10 rounded focus:outline-none focus:shadow-outline">Clear</button>
     </div>
   );
 };
